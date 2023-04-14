c6c73678f5ff0bcec4b9b95f6e00450862fc4c33
Md. Sazzad Hossain <96966545+Sazz07@users.noreply.github.com>
2023-04-14 13:27:04 +0600

    floder structure and 2section done

---
+++ README.md
@@ -0,0 +1,1 @@
+# orbido_task
---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Hero from "./Hero";
+
+export {
+    Navbar,
+    Billing,
+    Business,
+    Clients,
+    CTA,
+    Footer,
+    Hero
+};
